279eaf393cf9c07e2466b93d79dc86ddd34b780e
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-23 17:18:33 +0300

    Stacks and Queues Exercises

---
+++ 01. Stacks-and-Queues-Exercises/E05.Fashion Boutique.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E05.Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> clothes = new Stack<int>(input);
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            int countBox = 1;
+
+            while (clothes.Count > 0)
+            {
+                int currentCapacity = capacity;
+
+                while (currentCapacity > 0 && clothes.Count > 0)
+                {
+                    if (clothes.Peek() < currentCapacity)
+                    {
+                        int currentClothes = clothes.Pop();
+                        currentCapacity -= currentClothes;
+                    }
+                    else if (clothes.Peek() == currentCapacity)
+                    {
+                        int currentClotes = clothes.Pop();
+                        currentCapacity -= currentClotes;
+
+                        if (clothes.Count > 0)
+                        {
+                            countBox++;
+                        }
+                    }
+                    else if (clothes.Peek() > currentCapacity)
+                    {
+                        countBox++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(countBox);
+        }
+    }
+}
+
