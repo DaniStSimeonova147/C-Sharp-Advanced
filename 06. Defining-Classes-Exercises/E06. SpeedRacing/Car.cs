0db1ffd49978aa675a839ccb2aed7f431728b145
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-26 14:41:41 +0300

    Exercise 6

---
+++ 06. Defining-Classes-Exercises/E06. SpeedRacing/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelledDistance = 0.0;
+        }
+        
+        public void Drive(double kilometers)
+        {
+            double neededFule = kilometers * FuelConsumptionPerKilometer;
+
+            if (neededFule > FuelAmount)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                FuelAmount -= neededFule;
+                TravelledDistance += kilometers;
+            }
+        }
+    }
+}
---
+++ 06. Defining-Classes-Exercises/E06. SpeedRacing/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var cars = GetCars();
+
+            DriveCars(cars);
+            PrintCars(cars);
+        }
+        #region
+        private static List<Car> GetCars()
+        {
+            List<Car> carsInfo = new List<Car>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+                //"{model} {fuelAmount} {fuelConsumptionFor1km}"
+
+                string currentModel = input[0];
+                double currentFuleAmount = double.Parse(input[1]);
+                double currentFuelConsumption = double.Parse(input[2]);
+
+                carsInfo.Add(new Car(currentModel, currentFuleAmount, currentFuelConsumption));
+            }
+
+            return carsInfo;
+        }
+
+        private static void DriveCars(List<Car> cars)
+        {
+            //"Drive {carModel} {amountOfKm}"
+
+            string[] input = Console.ReadLine().Split();
+
+            while (input[0] != "End")
+            {
+                string model = input[1];
+                double amountOfKm = double.Parse(input[2]);
+
+                Car currentCar = cars.Where(c => c.Model == model).FirstOrDefault();
+
+                if (currentCar != null)
+                {
+                    currentCar.Drive(amountOfKm);
+                }
+
+                input = Console.ReadLine().Split();
+            }
+        }
+
+        private static void PrintCars(List<Car> cars)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, cars
+                .Select(c => $"{c.Model} {c.FuelAmount:f2} {c.TravelledDistance}")));
+        }
+        #endregion
+    }
+}
