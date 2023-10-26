195f3f819ec4ce4917635514bb5faaa575d06047
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-26 14:41:17 +0300

    Exercise 5

---
+++ 06. Defining-Classes-Exercises/E05. DateModifier/DateModifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public static void DateDifference (string firstDate, string secondDate)
+        {
+            int[] firstDateInput = firstDate.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondDateInput= secondDate.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            DateTime firstDateInfo = new DateTime(firstDateInput[0], firstDateInput[1], firstDateInput[2]);
+
+            DateTime secondDateInfo = new DateTime(secondDateInput[0], secondDateInput[1], secondDateInput[2]);
+
+            TimeSpan difference = firstDateInfo.Subtract(secondDateInfo);
+
+            Console.WriteLine(Math.Abs(difference.TotalDays));
+
+        }
+    }
+}
