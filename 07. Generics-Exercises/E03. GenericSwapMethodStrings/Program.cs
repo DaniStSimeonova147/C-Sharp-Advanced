2e30ae972c542e0e3a9e3909279534ecb43f00d5
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-27 15:33:31 +0300

    Exercise 3

---
+++ 07. Generics-Exercises/E03. GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace E03.GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> result = new Box<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string currentItem = Console.ReadLine();
+
+                result.Add(currentItem);
+            }
+
+            int[] token = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            result.Swap(token[0], token[1]);
+
+            Console.WriteLine(result.ToString()); 
+        }
+    }
+}
