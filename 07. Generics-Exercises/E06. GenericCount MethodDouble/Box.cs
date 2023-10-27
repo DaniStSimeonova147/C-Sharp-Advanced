ef84820ad443c0e602df78f4e67e9272055ee87c
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-27 15:34:49 +0300

    Exercise 6

---
+++ 07. Generics-Exercises/E06. GenericCount MethodDouble/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E06.GenericCount_MethodDouble
+{
+    public class Box<T> : IComparable<T>
+    where T : IComparable<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public int CompareTo(T other)
+        {
+            return this.value.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType().FullName}: {this.value}";
+        }
+    }
+
+}
