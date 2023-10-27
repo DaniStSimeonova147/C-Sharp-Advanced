574450b4bfe920cc0c87dfb21cc1f53d3218a8fa
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-27 15:33:56 +0300

    Exercise 4

---
+++ 07. Generics-Exercises/E04. GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E04.GenericSwapMethodIntegers
+{
+    class Box<Titem>
+    {
+        List<Titem> items = new List<Titem>();
+
+        public Box()
+        {
+            items = new List<Titem>();
+        }
+
+        public void Add(Titem item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (firstIndex <0 || secondIndex < 0 || firstIndex > items.Count || secondIndex > items.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Titem currentItem = items[firstIndex];
+
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = currentItem;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{item.GetType().FullName}: {item}");
+                
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 07. Generics-Exercises/E04. GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace E04.GenericSwapMethodIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> result = new Box<int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int currentItem = int.Parse(Console.ReadLine());
+
+                result.Add(currentItem);
+            }
+
+            int[] tokens = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            result.Swap(tokens[0], tokens[1]);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
