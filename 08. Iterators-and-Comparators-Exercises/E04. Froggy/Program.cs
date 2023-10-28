5da2bd5d91017abb6a6d9aeeeb0acc0772742c4c
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-28 17:59:04 +0300

    Froggy

---
+++ 08. Iterators-and-Comparators-Exercises/E04. Froggy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace E04.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse )
+                .ToArray();
+
+            Lake lake = new Lake(inputNumbers);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
