ae39e0f3254e626a04fa41483ffdaf3697d03f62
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-28 18:00:41 +0300

    Custom linked list

---
+++ 08. Iterators-and-Comparators-Exercises/E07. CustomLinkedList/Sorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace E07.CustomLinkedList
+{
+   public class Sorter
+    {
+        public static void Sort<T>(ref CustomList<T> list)
+        where T : IComparable<T>
+        {
+            List<T> sorted = list.OrderBy(e => e).ToList();
+
+            list = new CustomList<T>(sorted);
+        }
+    }
+}
