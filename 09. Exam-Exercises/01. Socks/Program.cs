c53b4f96b35e464864d84eddbe1239006c117029
Dani Simeonova <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-10-30 19:41:05 +0200

    Exam exercises

---
+++ 09. Exam-Exercises/01. Socks/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A01.Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] firstInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondInput = Console.ReadLine()
+              .Split()
+              .Select(int.Parse)
+              .ToArray();
+
+            Stack<int> leftSocks = new Stack<int>(firstInput);
+
+            Queue<int> rightSocks = new Queue<int>(secondInput);
+
+            List<int> result = new List<int>();
+
+            while (leftSocks.Count > 0 && rightSocks.Count > 0)
+            {
+                int currentLeftSocks = leftSocks.Peek();
+
+                int currentRightSocks = rightSocks.Peek();
+
+                if (currentLeftSocks > currentRightSocks)
+                {
+                    leftSocks.Pop();
+                    rightSocks.Dequeue();
+                    int pair = currentLeftSocks + currentRightSocks;
+                    result.Add(pair);
+                }
+                else if (currentLeftSocks < currentRightSocks)
+                {
+                    leftSocks.Pop();
+                }
+                else if (currentLeftSocks == currentRightSocks)
+                {
+                    int newCurrentLeftSocks = currentLeftSocks + 1;
+                    leftSocks.Pop();
+                    leftSocks.Push(newCurrentLeftSocks);
+                    rightSocks.Dequeue();
+                }
+            }
+            Console.WriteLine(result.Max());
+            Console.WriteLine(string.Join(" ", result));
+            
+        }
+    }
+}
---
+++ 09. Exam-Exercises/02. Repository/Repository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Repository
+{
+    class Repository
+    {
+        private Dictionary<int, Person> data;
+
+        private int id;
+
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+        public Repository()
+        {
+            data = new Dictionary<int, Person>();
+            id = 0;
+        }
+
+        public void Add(Person person)
+        {
+            data.Add(id, person);
+            id++;
+        }
+        public Person Get(int id)
+        {
+            return data[id];
+        }
+
+        public bool Update(int id, Person newPerson)
+        {
+            bool isValid = true;
+
+            if (!data.ContainsKey(id))
+            {
+                isValid = false;
+            }
+            else
+            {
+                data.Remove(id);
+                data[id] = newPerson;
+            }
+
+            return isValid;
+        }
+
+        public bool Delete(int id)
+        {
+            bool check = true;
+
+            if (!data.ContainsKey(id))
+            {
+                check = false;
+            }
+            else
+            {
+                data.Remove(id);
+            }
+
+
+            return check;
+        }
+    }
+}
